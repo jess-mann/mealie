ed7691e38c712257870b02c97e3e4e46794f5c5b
Miah <miah120@proton.me>
2026-07-28 20:44:15 +0000

    Breakup parse page into smaller parts

---
+++ frontend/app/components/Domain/Recipe/RecipePage/RecipePageParts/RecipeParseDialog/ParseDialogInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <SpinTransition>
+    <v-empty-state color="success" :icon="$globals.icons.checkboxMarkedCircle">
+      <div class="d-flex ga-2 flex-column">
+        <p>{{ $t("recipe.parser.ingredient-parser-description") }}</p>
+        <p>{{ $t("recipe.parser.ingredient-parser-final-review-description") }}</p>
+      </div>
+    </v-empty-state>
+  </SpinTransition>
+  <v-checkbox
+    v-model="dontShowAgain"
+    hide-details
+    density="compact"
+    :label="$t('recipe.parser.dont-show-again')"
+  />
+</template>
+
+<script setup lang="ts">
+const dontShowAgain = defineModel<boolean>({ default: true });
+</script>
